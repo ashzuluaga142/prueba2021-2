5e7857eb417c453fcfa0469cf82ad7e93707addc
ashzuluaga142 <79558235+ashzuluaga142@users.noreply.github.com>
2021-08-24 07:36:31 -0500

    Unite test Preparate

---
+++ Prueba2021-2.Tests/TestFactory.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using Prueba2021_2.Common.Models;
+using Prueba2021_2.Functions.Entities;
+using Prueba2021_2.Tests.Helpers;
+using System;
+using System.IO;
+
+namespace Prueba2021_2.Tests
+{
+    public class TestFactory
+    {
+        public static TodoEntity GetTodoEntity()
+        {
+            return new TodoEntity
+            {
+                ETag = "*",
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                createdTime = DateTime.UtcNow,
+                IsCompleted = false,
+                TaskDescription = "Task: kill the  humans"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid todoId, Todo todoRequest)
+        {
+            string request = JsonConvert.SerializeObject(todoRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamsFromString(request),
+                Path = $"/{todoId}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid todoId)
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Path = $"/{todoId}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Todo todoRequest)
+        {
+            string request = JsonConvert.SerializeObject(todoRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamsFromString(request),
+
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest()
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext());
+        }
+
+        public static Todo GetTodoRequest()
+        {
+            return new Todo
+            {
+                createdTime = DateTime.UtcNow,
+                IsCompleted = false,
+                TaskDescription = "Try to conquer the world."
+            };
+        }
+
+
+        public static Stream GenerateStreamsFromString(string stringToConvert)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(stringToConvert);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)// se debe modificar para cada ambiente de trabajo
+        {
+            ILogger logger;
+            if (type == LoggerTypes.list)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+            return logger;
+        }
+    }
+}
