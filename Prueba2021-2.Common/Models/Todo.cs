5dfd955d3696e248509a56aeb15f40ae0dac5f10
ashzuluaga142 <79558235+ashzuluaga142@users.noreply.github.com>
2021-08-23 20:57:31 -0500

    Add entity

---
+++ Prueba2021-2.Common/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba2021_2.Common.Models
+{
+   public class Todo
+    {
+        public DateTime createdTime { get; set; }
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Prueba2021-2.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba2021_2.Functions.Entities
+{
+    public class TodoEntity: TableEntity
+    {
+        public DateTime createdTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
